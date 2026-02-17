b57213ba59cece6fd477b6fbcb697e9777385d7b
Nia Savova <122923175+nks8566@users.noreply.github.com>
2026-02-16 18:09:30 -0600

    added and did minimum testing for constructor

--- CorpusReader_SLM.py
+++ CorpusReader_SLM.py
@@ -1,9 +1,77 @@
 
-
+import nltk
+from nltk.util import ngrams  
+from nltk.corpus import stopwords
+from nltk.stem import SnowballStemmer
+from collections import Counter
 
 class CorpusReader_SLM():
     def __init__(self, corpus, stopWord = "none", toStem = False, smooth = False, trigram = False):
-        pass
+        # corpus (required): a corpus object in NLTK
+        self.toStem = toStem
+        self.trigram_flag = trigram
+        # stopWord: what stopWords to remove
+        # “none”: no stopwords need to be removed
+        if stopWord == "none":
+            self.stopwords = set()
+        # “standard”: use the standard English stopWord available in NLTK
+        elif stopWord == "standard":
+            self.stopwords = set(stopwords.words('english'))
+        # Others: the input should be treated as a filename where stopwords are to be read. You
+        # should assume any word inside the stopwords file is a stopword. You should assume the
+        # file is being stored in the same directory as where the program is run.
+        # Notice all stopwords (as in the corpus) should be converted to lower case (see below)
+        else:
+            with open(stopWord, 'r') as f:
+                self.stopwords = set(line.strip().lower() for line in f)
+        # toStem: if true, use the Snowball stemmer to stem the words beforehand.
+        if toStem:
+            self.stemmer = SnowballStemmer("english")
+        else:
+            self.stemmer = None
+        # initialize counters
+        self.unigram_counts = Counter()
+        self.bigram_counts = Counter()
+        self.trigram_counts = Counter() # only used if trigram=True
+        for sent in corpus.sents():
+            processed_sent = []
+            for word in sent:
+                word = word.lower()
+                if word not in self.stopwords:
+                    if self.stemmer:
+                        word = self.stemmer.stem(word)
+                    processed_sent.append(word)
+            # Update ngram counts
+            self.unigram_counts.update(processed_sent)
+            self.bigram_counts.update(ngrams(processed_sent, 2))
+            if trigram:
+                self.trigram_counts.update(ngrams(processed_sent, 3))
+        # smooth: if true, use Laplace (+1) smoothing to calculate the probabilities.
+        if smooth:
+            self.vocab = set(self.unigram_counts.keys())
+            V = len(self.vocab)
+            N = sum(self.unigram_counts.values())
+            # probabilities with Laplace
+            self.unigram_probs = {}
+            for word in self.vocab:
+                self.unigram_probs[word] = (self.unigram_counts[word] + 1) / (N + V)
+            self.bigram_probs = {}
+            for bigram, count in self.bigram_counts.items():
+                bigram_str = " ".join(bigram)
+                self.bigram_probs[bigram_str] = (count + 1) / (self.unigram_counts[bigram[0]] + V)
+            # trigram: if true, calculate and store trigram probabilities in addition to unigram and bigrams.
+            if trigram:
+                self.trigram_probs = {}
+                for trigram, count in self.trigram_counts.items():
+                    trigram_str = " ".join(trigram)
+                    bigram_str = " ".join(trigram[:2])
+                    self.trigram_probs[trigram_str] = (count + 1) / (self.bigram_counts[bigram_str] + V)
+        else:
+            # probabilities without smoothing
+            self.unigram_probs = {word: count / sum(self.unigram_counts.values()) for word, count in self.unigram_counts.items()}
+            self.bigram_probs = {" ".join(bigram): count / self.unigram_counts[bigram[0]] for bigram, count in self.bigram_counts.items()}
+            if trigram:
+                self.trigram_probs = {" ".join(trigram): count / self.bigram_counts[" ".join(trigram[:2])] for trigram, count in self.trigram_counts.items()}
 
     # Class methods
     def unigram(count = 0):
@@ -23,4 +91,3 @@ class CorpusReader_SLM():
 
     def trigramGenerate(code = 0, head= []):
         pass
-
---
+++ constructor_tester.py
@@ -0,0 +1,30 @@
+
+from CorpusReader_SLM import CorpusReader_SLM
+import nltk
+from nltk.corpus import brown
+
+# Make sure the required corpora are downloaded
+nltk.download('brown')
+nltk.download('stopwords')
+
+# Use the Brown corpus as a test
+corpus = brown
+
+# Create your CorpusReader_SLM object
+reader = CorpusReader_SLM(corpus, stopWord="standard", toStem=False, smooth=True, trigram=True)
+
+# Print some basic checks
+print("Most common unigrams:")
+for word, count in reader.unigram_counts.most_common(10):
+    print(word, count, round(reader.unigram_probs[word], 4))
+
+print("\nMost common bigrams:")
+for bigram, count in list(reader.bigram_counts.most_common(10)):
+    bigram_str = " ".join(bigram)
+    print(bigram_str, count, round(reader.bigram_probs[bigram_str], 4))
+
+if reader.trigram_flag:
+    print("\nMost common trigrams:")
+    for trigram, count in list(reader.trigram_counts.most_common(10)):
+        trigram_str = " ".join(trigram)
+        print(trigram_str, count, round(reader.trigram_probs[trigram_str], 6))
